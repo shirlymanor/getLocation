0daa9d692c55d08b32d2eef8e396000d3dd4e7f6
Shirly Manor <shirly.manor@gmail.com>
2017-02-12 15:09:07 -0800

    Initial Commit

---
+++ getLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  getLocation
+//
+//  Created by Shirly Manor on 2/12/17.
+//  Copyright © 2017 manor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
